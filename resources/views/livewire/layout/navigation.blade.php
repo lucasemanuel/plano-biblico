32795f0cd94741acbb737a9636ca1a39f91a1d4f
Lucas Emanuel <lucaxemanuel@gmail.com>
2023-12-21 17:00:19 -0300

    add family fonts

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -18,7 +18,7 @@ $logout = function (Logout $logout) {
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}" wire:navigate>
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <x-application-logo class="block h-9 leading-9 w-auto fill-current text-xl text-gray-800 font-black" />
                     </a>
                 </div>
 
