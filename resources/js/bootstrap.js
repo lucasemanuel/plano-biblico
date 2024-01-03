b3968c13edbbd2ba7b2d946442a701f521295b15
Lucas Emanuel <lucaxemanuel@gmail.com>
2024-01-03 09:26:19 -0300

    set locale

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,6 +10,15 @@ window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
+ * Define Dayjs global
+ */
+import 'dayjs/locale/pt-br';
+import dayjs from 'dayjs';
+const appLocale = import.meta.env.VITE_APP_LOCALE || 'en';
+
+dayjs.locale(appLocale);
+
+/**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
