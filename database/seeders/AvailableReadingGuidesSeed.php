b24bee86d1b22cc8ddd64b5d2e47b6e8ad621291
Lucas Emanuel <lucaxemanuel@gmail.com>
2024-01-04 23:16:50 -0300

    seed

---
+++ database/seeders/AvailableReadingGuidesSeed.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\AvailableReadingGuide;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AvailableReadingGuidesSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        AvailableReadingGuide::create([
+            'name' => 'Ano Bíblico - 2024',
+            'version' => '1.0.0',
+            'path_csv' => storage_path('csv') . '/ano-biblico.csv'
+        ]);
+    }
+}
