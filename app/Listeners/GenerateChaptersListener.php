e8f0d3ab887e05274cae464e92320a65747823a9
Lucas Emanuel <lucaxemanuel@gmail.com>
2024-01-04 23:17:53 -0300

    refactor process register reading guide

--- app/Listeners/GenerateChaptersListener.php
+++ app/Listeners/GenerateChaptersListener.php
@@ -2,7 +2,9 @@
 
 namespace App\Listeners;
 
-use Illuminate\Support\Arr;
+use App\Models\AvailableReadingGuide;
+use Exception;
+use Illuminate\Support\Facades\DB;
 use Spatie\SimpleExcel\SimpleExcelReader;
 
 class GenerateChaptersListener
@@ -20,29 +22,121 @@ class GenerateChaptersListener
      */
     public function handle(object $event): void
     {
-        $path = storage_path('csv') . '/ano-biblico.csv';
-        $rows = SimpleExcelReader::create($path)->getRows();
-        $rows = $rows->groupBy('mes')->collect();
+        $user = $event->user;
+        $guideBase = AvailableReadingGuide::latest()->first();
+        $excerpts = $this->makeExcerpts($guideBase);
 
-        $chapterDays = $rows->map(function ($month) {
-            $month = $month->groupBy('dia');
-            return $month->map(function ($chapter) {
-                $meta = $chapter->reduce(function ($carry, $item) {
+        DB::beginTransaction();
+        try {
+            $guide = $user->readingGuides()->create([
+                'available_reading_guide_id' => $guideBase->id,
+            ]);
+            $guide->excerpts()->createMany($excerpts);
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            throw $th;
+        }
+    }
+
+    private function makeExcerpts(AvailableReadingGuide $guideBase)
+    {
+        $rowsCsv = SimpleExcelReader::create($guideBase->path_csv)->getRows();
+        $rows = $rowsCsv->groupBy('section')->collect();
+
+        return $rows->map(function ($section) {
+            $section = $section->groupBy('index');
+            return $section->map(function ($excerpt) {
+                $chapters = [];
+                $meta = $excerpt->reduce(function ($carry, $item) use (&$chapters) {
                     $carry[] = [
-                        'book' => $item['livro'],
-                        'chapter' => $item['capitulo'],
-                        'verses' => empty($item['versos']) ? null : $item['versos']
+                        'book' => $item['book'],
+                        'chapter' => $item['chapter'],
                     ];
+                    $chapters[] = $item['chapter'];
                     return $carry;
                 });
+                $title = $this->replaceAbbrToFullName($excerpt[0]['book']) . ' ' . collect($chapters)->join('-');
                 return [
-                    'day' => $chapter[0]['dia'],
-                    'month' => $chapter[0]['mes'],
+                    'key' => $excerpt[0]['index'],
+                    'section' => $excerpt[0]['section'],
+                    'title' => $title,
                     'meta' => $meta
                 ];
             });
         })->collapse();
+    }
 
-        $event->user->chapterDays()->createMany($chapterDays);
+    private function replaceAbbrToFullName(string $abbr)
+    {
+        return match ($abbr) {
+            "gn" => "Gênesis",
+            "ex" => "Êxodo",
+            "lv" => "Levítico",
+            "nm" => "Números",
+            "dt" => "Deuteronômio",
+            "js" => "Josué",
+            "jz" => "Juízes",
+            "rt" => "Rute",
+            "1sm" => "1 Samuel",
+            "2sm" => "2 Samuel",
+            "1rs" => "1 Reis",
+            "2rs" => "2 Reis",
+            "1cr" => "1 Crônicas",
+            "2cr" => "2 Crônicas",
+            "ed" => "Esdras",
+            "ne" => "Neemias",
+            "et" => "Ester",
+            "job" => "Jó",
+            "sl" => "Salmos",
+            "pv" => "Provérbios",
+            "ec" => "Eclesiastes",
+            "ct" => "Cânticos",
+            "is" => "Isaías",
+            "jr" => "Jeremias",
+            "lm" => "Lamentações",
+            "ez" => "Ezequiel",
+            "dn" => "Daniel",
+            "os" => "Oséias",
+            "jl" => "Joel",
+            "am" => "Amós",
+            "ob" => "Obadias",
+            "jn" => "Jonas",
+            "mq" => "Miquéias",
+            "na" => "Naum",
+            "hc" => "Habacuque",
+            "sf" => "Sofonias",
+            "ag" => "Ageu",
+            "zc" => "Zacarias",
+            "ml" => "Malaquias",
+            "mt" => "Mateus",
+            "mc" => "Marcos",
+            "lc" => "Lucas",
+            "jo" => "João",
+            "at" => "Atos",
+            "rm" => "Romanos",
+            "1co" => "1 Coríntios",
+            "2co" => "2 Coríntios",
+            "gl" => "Gálatas",
+            "ef" => "Efésios",
+            "fp" => "Filipenses",
+            "cl" => "Colossenses",
+            "1ts" => "1 Tessalonicenses",
+            "2ts" => "2 Tessalonicenses",
+            "1tm" => "1 Timóteo",
+            "2tm" => "2 Timóteo",
+            "tt" => "Tito",
+            "fm" => "Filemon",
+            "hb" => "Hebreus",
+            "tg" => "Tiago",
+            "1pe" => "1 Pedro",
+            "2pe" => "2 Pedro",
+            "1jo" => "1 João",
+            "2jo" => "2 João",
+            "3jo" => "3 João",
+            "jd" => "Judas",
+            "ap" => "Apocalipse",
+            default => throw new Exception('Book Not found' . $abbr),
+        };
     }
 }
